ecbfdbb2855e1de32e005e272806c64c258b2b8f
903958734@qq.com <903958734@qq.com>
2020-11-01 16:15:02 +0800

    添加一键部署脚本

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+yarn build && 
+git chekout gh-pages &&
+rm -rf *html *.css *.png src *.sh *.js *json yarn.lock && 
+mv dist/* ./ && 
+rm -rf dist; 
+git add . &&
+git commit -m 'update' &&
+git push
+git chekout -
