ad98455b38bf410e83e0b3abf77b3e4d7363d6c5
903958734@qq.com <903958734@qq.com>
2020-11-01 16:18:37 +0800

    update

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-yarn build && 
-git chekout gh-pages &&
-rm -rf *html *.css *.png src *.sh *.js *json yarn.lock && 
-mv dist/* ./ && 
-rm -rf dist; 
-git add . &&
-git commit -m 'update' &&
-git push
-git chekout -
